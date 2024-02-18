397108e18b67441d6c98dd3bc3f58a6ab921a577
jeanhua <2207739460@qq.com>
2024-02-18 15:45:27 +0800

    中文编码问题

--- 2和1024.c
+++ 2和1024.c
@@ -2,7 +2,7 @@
 #include<math.h>
 int main()
 {
-	printf("ÇëÊäÈëÊı¾İ\n");
+	printf("è¯·è¾“å…¥æ•°æ®\n");
 	double input;
 	scanf_s("%lf", &input);
 	if (log(input) / log(2.0) - (int)((log(input) / log(2))) != 0 && (int)input % 1024 != 0)
